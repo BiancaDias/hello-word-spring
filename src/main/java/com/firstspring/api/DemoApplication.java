6271937e17c2db234df3f02e335b63a5c39527a5
Bianca Dias <bianca.dias@ufv.br>
2023-11-11 13:49:45 -0300

    feat: get hello world

--- src/main/java/com/firstspring/api/DemoApplication.java
+++ src/main/java/com/firstspring/api/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.firstspring.demo;
+package com.firstspring.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
